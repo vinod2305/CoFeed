11a075c200d248e79592575144697bb32abf8335
Vinod S <s.vinod52@gmail.com>
2021-06-22 14:49:39 +0530

    Posts - create,get,delete,update,like/dislike added

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users")
 const authRoute = require("./routes/auth")
+const postRoute = require("./routes/posts");
 
 dotenv.config();
 
@@ -25,7 +26,7 @@ app.use(morgan("common"));
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
-
+app.use("/api/posts", postRoute);
 
 
 app.listen(8800, () => {
