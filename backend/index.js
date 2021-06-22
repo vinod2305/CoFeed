130eaef97b07bbe18388776f1e2d7cc4237c610a
Vinod S <s.vinod52@gmail.com>
2021-06-22 12:00:40 +0530

    inital commit

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to mongoDB");
+  }
+);
+
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
+
+
+app.listen(8800, () => {
+  console.log("Server running...");
+});
---
+++ backend/routes/users.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const User = require("../models/User");
+
+//update user
+
+
+module.exports = router;
