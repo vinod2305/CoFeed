3b3ea1220375e53552339ec7845bc72cc4f2808b
Vinod S <s.vinod52@gmail.com>
2021-06-23 13:21:22 +0530

    Added home page with right bar, feed and side bar

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,3 +1,18 @@
-export default function Home(){
-    return
+import Sidebar from "../../components/sidebar/Sidebar";
+import Topbar from "../../components/topbar/Topbar";
+import Rightbar from "../../components/rightbar/Rightbar";
+import Feed from "../../components/feed/Feed";
+import "./home.scss";
+
+export default function Home() {
+  return (
+    <>
+      <Topbar />
+      <div className="homeContainer">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
+    </>
+  );
 }
