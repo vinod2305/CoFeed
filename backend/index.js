8d5dc00192f5128c9ae8d035930aafdd8ca06f43
Vinod S <s.vinod52@gmail.com>
2021-06-25 13:53:36 +0530

    Fixed like and dislike, follow and unfolow users

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,11 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const userRoute = require("./routes/users")
-const authRoute = require("./routes/auth")
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const multer = require("multer");
+const path = require("path");
 
 dotenv.config();
 
@@ -18,16 +20,33 @@ mongoose.connect(
   }
 );
 
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
-
 
 app.listen(8800, () => {
   console.log("Server running...");
